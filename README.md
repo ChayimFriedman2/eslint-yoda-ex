e32843a297167b51e28375c565df1bfb28f19d10
Chayim Refael Friedman <chayimfr@gmail.com>
2020-05-14 10:59:13 +0300

    Scaffold rule

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # eslint-yoda-ex
 
 ESLint plugin for more comprehensive yoda rule
+
+### Rules:
+
+ - [yoda-ex](./docs/rules/yoda-ex.md)
---
+++ src/rules/yoda-ex.js
@@ -0,0 +1,44 @@
+/**
+ * @fileoverview Like ESLint core yoda rules but with more options
+ * @author Chayim Refael Friedman
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Rule Definition
+//------------------------------------------------------------------------------
+
+module.exports = {
+    meta: {
+        docs: {
+            description: "Like ESLint core yoda rules but with more options",
+            category: "Fill me in",
+            recommended: false
+        },
+        fixable: null,  // or "code" or "whitespace"
+        schema: [
+            // fill in your schema
+        ]
+    },
+
+    create: function(context) {
+
+        // variables should be defined here
+
+        //----------------------------------------------------------------------
+        // Helpers
+        //----------------------------------------------------------------------
+
+        // any helper functions should go here or else delete this section
+
+        //----------------------------------------------------------------------
+        // Public
+        //----------------------------------------------------------------------
+
+        return {
+
+            // give me methods
+
+        };
+    }
+};
